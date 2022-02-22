3b841d3f908ad45502f9c33c1be410a090d7ae77
Aida Queralt <aidaqueraltt@gmail.com>
2022-02-22 16:10:03 +0100

    hileras ladrillos

--- game.py
+++ game.py
@@ -1,52 +1,129 @@
 import pygame as pg
-import random
+import random 
 pg.init()
 
+class Vigneta:
+    def __init__(self, padre, x, y, ancho, alto, color = (12, 183, 242)):
+        self.padre = padre
+        self.x = x
+        self.y = y
+        self.color = color
+        self.ancho = ancho
+        self.alto = alto
+        self.vx = 0
+        self.vy = 0
+
+    def dibujar(self):
+        pass
+
+    def mover(self):
+        pass
+
+class Ladrillo(Vigneta):
+    def dibujar(self):
+        pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
+
+    def comprobarToque(self, bola):
+        #hago cosas al tocarme la bola
+        pass
+
+
+class Raqueta(Vigneta):
+    def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 255)):
+        super().__init__(padre, x, y, ancho, alto, color)
+        self.vx = 5
+
+    def dibujar(self):
+        pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
+
+    def mover(self):
+        teclas = pg.key.get_pressed()
+        if teclas[pg.K_LEFT]:
+            self.x -= self.vx
+        if teclas[pg.K_RIGHT]:
+            self.x += self.vx
+
+        if self.x <= 0:
+            self.x = 0
+        if self.x + self.ancho >= self.padre.get_width():
+            self.x = self.padre.get_width() - self.ancho
+
 class Bola:
-    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10):
+    def __init__(self, padre, x, y, color = (0, 255, 0), radio = 10) :
         self.x = x
         self.y = y
         self.color = color
         self.radio = radio
-        self.vx = 1
-        self.vy = 1
+        self.vx = -100
+        self.vy = -100
         self.padre = padre
 
 
-    
     def mover(self):
-        self.x += self.vx
+        self.x += self.vx 
         self.y += self.vy
 
-        if self.x <= self.radio or self.x >= self.padre.get_width()  - self.radio:
+        if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio:
             self.vx *= -1
-        
-        if self.y <= self.radio or self.y  >= self.padre.get_height()- self.radio:
+
+        #if self.radio >= self.x >= limDer - self.radio:
+
+        if self.y <= self.radio or self.y >= self.padre.get_height() - self.radio:
             self.vy *= -1
-    
+
     def dibujar(self):
         pg.draw.circle(self.padre, self.color, (self.x, self.y), self.radio)
+
+    def compruebaChoque(self, otro):
+        if (self.x - self.radio in range(otro.x, otro.x + otro.ancho) or \
+            self.x + self.radio in range(otro.x, otro.x + otro.ancho)) and \
+           (self.y - self.radio in range(otro.y, otro.y + otro.alto) or \
+            self.y + self.radio in range(otro.y, otro.y + otro.alto)):
+           
+           self.vy *= -1
 
 
 
 class Game:
     def __init__(self, ancho=600, alto=800):
         self.pantalla = pg.display.set_mode((ancho, alto))
-        self.bola = Bola(ancho // 2, alto // 2, (255, 255, 0))
-        self.bolas = []
-        self.aleatorio = Bola(ancho // 2, alto // 2, (255, 255, 0))
-        
+        self.bola = Bola(self.pantalla, ancho // 2, alto //2 )
+        self.raqueta = Raqueta(self.pantalla, ancho//2, alto // 2, 100, 20)
 
-        for i in range(100):
-            aleatorio = random.randint(1, 100)
-            self.bolas.append(aleatorio)
-            
-            
-            
+        self.ladrillos = []
+        self.posicion_x_ladrillo = 1
+        for i in range(6):
+            self.ladrillo = Ladrillo(self.pantalla, self.posicion_x_ladrillo, 10, 96, 20)
+            self.ladrillos.append(self.ladrillo)
+            self.posicion_x_ladrillo += 100
         
+        self.segunda_hilera = []
+        self.posicion_x_segunda_hilera = -40
+        self.posicion_y_segunda_hilera = 35
+        for i in range(7):
+            self.segundo_ladrillo = Ladrillo(self.pantalla, self.posicion_x_segunda_hilera, self.posicion_y_segunda_hilera, 96, 20)
+            self.segunda_hilera.append(self.segundo_ladrillo)
+            self.posicion_x_segunda_hilera += 100
             
+
+
+
         
-    
+        """
+        for i in range(random.randint(2, 10)):
+            radio = random.randint(5, 50)
+            self.bolas.append(Bola(random.randint(radio, ancho - radio),
+                                   random.randint(radio, alto - radio),
+                                   self.pantalla,
+                                   (random.randint(0, 255),
+                                    random.randint(0, 255),
+                                    random.randint(0, 255)),
+                                   radio))
+            self.bolas[i].vx = random.randint(5,15) * random.choice[(-1, 1)]
+            self.bolas[i].vy = random.randint(5,15) * random.choice[(-1, 1)]
+        """
+
+
     def bucle_ppal(self):
         game_over = False
 
@@ -56,20 +133,41 @@ class Game:
             for evento in eventos:
                 if evento.type == pg.QUIT:
                     game_over = True
+                """
+                if evento.type == pg.KEYDOWN:
+                    if evento.key == pg.K_LEFT:
+                        self.raqueta.vx = -5
 
+                    if evento.key == pg.K_RIGHT:
+                        self.raqueta.vx = 5
 
-            # mover todas las bolas
-            self.bola.mover()
-            self.aleatorio.mover()
-            self.pantalla.fill((255, 0, 0))
-            #dibujar todas las bolas
-            self.bola.dibujar()
-            self.aleatorio.dibujar()
-           
+                if evento.type == pg.KEYUP:
+                    if evento.key in (pg.K_LEFT, pg.K_RIGHT):
+                        self.raqueta.vx = 0
+                """
             
+
+            self.pantalla.fill((0, 0, 128))    
+
+            self.ladrillo.mover()
+            self.bola.mover()
+            self.raqueta.mover()
+            self.bola.compruebaChoque(self.raqueta)
+            self.bola.dibujar()
+            self.raqueta.dibujar()
+
+            for i in range(len(self.ladrillos)):
+                self.ladrillos[i].dibujar()
+            
+            for i in range(len(self.segunda_hilera)):
+                self.segunda_hilera[i].dibujar()
+            
+
+            
+
             pg.display.flip()
 
-
+    
 if __name__ == '__main__':
     pg.init()
 
@@ -77,3 +175,4 @@ if __name__ == '__main__':
     game.bucle_ppal()
 
     pg.quit()
+    
--- game2.py
+++ game2.py
@@ -3,8 +3,22 @@ import random
 pg.init()
 
 
+class Raqueta:
+    def __init__(self, padre, x, y, ancho, alto, color = (255, 200, 85)):
+        self.padre = padre
+        self.x = x
+        self.y = y
+        self.color = color
+        self.ancho = ancho
+        self.alto = alto
+        self.vx = 5
+        self.vy = 0
+
+    def dibujar(self):
+        pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
+
 class Bola:
-    def __init__(self, x, y, padre,  color = (255, 255, 255), radio = 10):
+    def __init__(self, x, y, padre,  color = (255, 255, 255), radio = 10, vx = 5, vy = 0):
         self.x = x
         self.y = y
         self.color = color
@@ -18,6 +32,7 @@ class Bola:
         self.x += self.vx 
         self.y += self.vy
 
+
         if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio:
             self.vx *= -1
 
@@ -29,19 +44,23 @@ class Bola:
     def dibujar(self):
         pg.draw.circle(self.padre, self.color, (self.x, self.y), self.radio)
 
+
+
 class Game:
     def __init__(self, ancho=600, alto=800):
         self.pantalla = pg.display.set_mode((ancho, alto))
         self.bola = Bola(ancho // 2, alto // 2, self.pantalla, (255, 255, 0))
+        self.raqueta = Raqueta(self.pantalla, ancho//2, alto - 30, 100, 25)
+
+        """
         self.bolas = []
-        
         num_bolas = random.randint(0, 50)
         for i in range(num_bolas):
             self.nueva_bola = Bola(random.randint(0,600), random.randint(0,800), self.pantalla, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(10, 35))
-            self.nueva_bola.vx = random.randint(-500, -120)
-            self.nueva_bola.vy = random.randint(-710, -250)
+            self.nueva_bola.vx = random.randint(-5, 1)
+            self.nueva_bola.vy = random.randint(-5, 1)
             self.bolas.append(self.nueva_bola)
-        
+        """
         
         
     def bucle_ppal(self):
@@ -54,19 +73,33 @@ class Game:
                 if evento.type == pg.QUIT:
                     game_over = True
 
-            #mover todas las bolas
-            self.bola.mover()
+                if evento.type == pg.KEYDOWN:
+                    if evento.key == pg.K_LEFT:
+                        self.raqueta.vx = -5
 
-            for i in range(len(self.bolas)):
-                self.bolas[i].mover()
+                    if evento.key == pg.K_RIGHT:
+                        self.raqueta.vx = 5
                 
+                if evento.type == pg.KEYUP:
+                    if evento.key in (pg.K_LEFT)
+
+
+
+            
+            
+
+        
+            #for i in range(len(self.bolas)):
+                #self.bolas[i].mover()
+
                 
             self.pantalla.fill((255, 0, 0))    
-            #dibujar todas las bolas
+            self.bola.mover()
             self.bola.dibujar()
+            self.raqueta.dibujar()
             
-            for i in range(len(self.bolas)):
-                self.bolas[i].dibujar()
+            #for i in range(len(self.bolas)):
+                #self.bolas[i].dibujar()
             
             
             pg.display.flip()
